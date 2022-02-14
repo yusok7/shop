51a5755b4f0b84473c1e635752a0bfd70371cd8e
yusok7 <diddbtjr7@naver.com>
2022-02-14 16:48:12 +0900

    Thymeleaf th:each 예제

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -30,4 +30,21 @@ public class ThymeleafExController {
         model.addAttribute("itemDto", itemDto);
         return "thymeleafEx/thymeleafEx02";
     }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            ItemDto itemDto = ItemDto.builder()
+                    .itemDetail("상품 상세 설명" + i)
+                    .itemNm("테스트 상품" + i)
+                    .price(1000 * i)
+                    .build();
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
 }
