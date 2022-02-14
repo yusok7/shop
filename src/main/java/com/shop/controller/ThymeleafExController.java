db89fb87140fbdb82189e8be868c7f91b1f62421
yusok7 <diddbtjr7@naver.com>
2022-02-14 16:34:18 +0900

    Thymeleaf th:text 예제

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,13 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -13,5 +17,17 @@ public class ThymeleafExController {
     public String thymeleafExample01(Model model) {
         model.addAttribute("data", "타임리프 예제 입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = ItemDto.builder()
+                .itemDetail("상품 상세 설명")
+                .itemNm("테스트 상품1")
+                .price(10000)
+                .build();
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
