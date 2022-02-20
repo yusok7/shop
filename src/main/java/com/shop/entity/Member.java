0cb2bd5e377df576f8d7642c7b2a7976fcd95ee1
yusok7 <diddbtjr7@naver.com>
2022-02-20 16:11:03 +0900

    회원 Entity 작성

---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,41 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "member")
+@Entity
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String name, String email, String password, String address, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.role = role;
+    }
+}
