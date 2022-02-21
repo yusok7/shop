efc3d2a08bd8a1d391703e7a71f825d6fc24582c
yusok7 <diddbtjr7@naver.com>
2022-02-21 16:22:32 +0900

    MemberController 작성

---
+++ src/main/java/com/shop/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.shop.controller;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/members")
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "/member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto) {
+        memberService.saveMember(memberFormDto.toEntity());
+        return "redirect:/";
+    }
+}
