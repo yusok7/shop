fdc77f8ff7357cf36551c60d9a020322897c05c5
yusok7 <diddbtjr7@naver.com>
2022-02-22 16:40:55 +0900

    MemberDto validation 추가

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -6,17 +6,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class MemberFormDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     @Builder
