6ad3ff2d37db58a10715cb201cb832063416796d
yusok7 <diddbtjr7@naver.com>
2022-02-24 21:13:34 +0900

    장바구니 상품 Entity

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,37 @@
+package com.shop.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "cart_item")
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+
+    @Builder
+    public CartItem(Cart cart, Item item, int count) {
+        this.cart = cart;
+        this.item = item;
+        this.count = count;
+    }
+}
