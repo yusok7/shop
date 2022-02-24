b5b79b20e1fb3094d691e8927443b52c38dc9e4f
yusok7 <diddbtjr7@naver.com>
2022-02-24 16:13:03 +0900

    상품 등록 테스트 작성

---
+++ src/test/java/com/shop/controller/ItemControllerTest.java
@@ -0,0 +1,42 @@
+package com.shop.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 등록 페이지 권한 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void itemFormTest() throws Exception {
+        mockMvc.perform(get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("상품 등록 페이지 일반 회원 접근 테스트")
+    @WithMockUser(username = "user", roles = "USER")
+    void itemFormNotAdminTest() throws Exception {
+        mockMvc.perform(get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
