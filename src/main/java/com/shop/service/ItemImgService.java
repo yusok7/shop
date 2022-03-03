ebd28dac209c05e41c352a7e1a7071dee79564bb
yusok7 <diddbtjr7@naver.com>
2022-03-03 16:23:42 +0900

    상품 이미지 업로드 / 이미지 정보 저장

---
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -0,0 +1,38 @@
+package com.shop.service;
+
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ItemImgService {
+
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception {
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        if (!StringUtils.isEmpty(oriImgName)) {
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item/" + imgName;
+        }
+
+        // 상품 이미지 정보 저장
+        itemImg.updateItemmg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+}
