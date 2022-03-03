4e930db8ad18cf6cd5a7e70207886a68d258f6c1
yusok7 <diddbtjr7@naver.com>
2022-03-03 16:16:23 +0900

    파일 처리 class

---
+++ src/main/java/com/shop/service/FileService.java
@@ -0,0 +1,35 @@
+package com.shop.service;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
+
+@Log
+@Service
+public class FileService {
+
+    public String uploadFile(String uploadPath, String originalFileName, byte[] fileData) throws Exception {
+        UUID uuid = UUID.randomUUID();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String savedFileName = uuid.toString() + extension;
+        String fileUploadFullUrl = uploadPath + "/" + savedFileName;
+        FileOutputStream fos = new FileOutputStream(fileUploadFullUrl);
+        fos.write(fileData);
+        fos.close();
+        return savedFileName;
+    }
+
+    public void deleteFile(String filePath) throws Exception {
+        File deletefile = new File(filePath);
+
+        if (deletefile.exists()) {
+            deletefile.delete();
+            log.info("파일을 삭제하였습니다.");
+        } else {
+            log.info("파일이 존재하지 않습니다.");
+        }
+    }
+}
