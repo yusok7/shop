e009c8506729023c88a6511751ae3ba000a6644d
yusok7 <diddbtjr7@naver.com>
2022-03-10 17:42:39 +0900

    상품 데이터 정보 전달 DTO 주석 추가

--- src/main/java/com/shop/dto/ItemFormDto.java
+++ src/main/java/com/shop/dto/ItemFormDto.java
@@ -13,6 +13,10 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 상품 데이터 정보를 전달하는 DTO
+ */
+
 @Getter @Setter
 @NoArgsConstructor
 public class ItemFormDto {
@@ -36,8 +40,11 @@ public class ItemFormDto {
     // 상품 저장 후 수정할 때 상품 이미지 정보를 저장하는 리스트
     private List<ItemImgDto> itemImgDtoList = new ArrayList<>();
 
-    // 상품의 이미지 아이디를 저장하는 리스트
-    // 수정 시 이미지 아이디를 담아둘 용도로 사용
+    /**
+     * 상품의 이미지 아이디를 저장하는 리스트
+     * 상품 등록 시에는 아직 상품의 이미지를 저장하지 않았기 때문에 아무 값도 들어가 있지 않고,
+     * 수정 시에 이미지 아이디를 담아둘 용도로 사용
+     */
     private List<Long> itemImgIds = new ArrayList<>();
 
     private static ModelMapper modelMapper = new ModelMapper();
