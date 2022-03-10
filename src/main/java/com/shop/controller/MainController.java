485f318eb36d515cb27a19a639e4dc8de064c648
yusok7 <diddbtjr7@naver.com>
2022-03-10 17:00:52 +0900

    MainPage api 수정

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String main() {
+    private final ItemService itemService;
+
+    @GetMapping(value = "/")
+    public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model) {
+        PageRequest pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
         return "main";
     }
 }
