b87da18ec45ec88ace78feb2995e9b175af41f92
yusok7 <diddbtjr7@naver.com>
2022-03-16 20:57:47 +0900

    주문 상품 정보 Dto

---
+++ src/main/java/com/shop/dto/OrderItemDto.java
@@ -0,0 +1,21 @@
+package com.shop.dto;
+
+import com.shop.entity.OrderItem;
+
+public class OrderItemDto {
+
+    private String itemNm; // 상품명
+
+    private int count; // 주문 수량
+
+    private int orderPrice; // 주문 금액
+
+    private String imgUrl; // 상품 이미지 경로
+
+    public OrderItemDto(OrderItem orderItem, String imgUrl) {
+        this.itemNm = orderItem.getItem().getItemNm();
+        this.count = orderItem.getCount();
+        this.orderPrice = orderItem.getOrderPrice();
+        this.imgUrl = imgUrl;
+    }
+}
