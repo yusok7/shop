f537cbdea643d6fd7c8c716fe16b7b8563924477
yusok7 <diddbtjr7@naver.com>
2022-03-30 17:47:29 +0900

    설명용 주석 추가 (공부 용도)

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,20 +1,28 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
+import com.shop.dto.OrderHistDto;
 import com.shop.entity.Item;
 import com.shop.entity.Member;
 import com.shop.entity.Order;
 import com.shop.entity.OrderItem;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * 주문 로직을 작성하기 위한 OrderService 클래스
+ */
 
 @RequiredArgsConstructor
 @Transactional
@@ -24,19 +32,36 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
+    /**
+     * @param orderDto 주문할 상품의 id와 주문 수량
+     * @param email
+     * @return
+     */
     public Long order(OrderDto orderDto, String email) {
+        // 1. 주문할 상품을 조회
         Item item = itemRepository.findById(orderDto.getItemId())
                 .orElseThrow(EntityNotFoundException::new);
+        // 2. 현재 로그인한 회원의 이메일 정보를 이용해서 회원 정보를 조회한다.
         Member member = memberRepository.findByEmail(email);
 
         List<OrderItem> orderItemList = new ArrayList<>();
+        // 3. 주문할 상품 엔티티와 주문 수량을 이용하여 주문 상품 엔티티를 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
         orderItemList.add(orderItem);
 
+        // 4. 회원 정보와 주문할 상품 리스트 정보를 이용하여 주문 엔티티를 생성한다.
         Order order = Order.createOrder(member, orderItemList);
+        // 5. 생성한 주문 엔티티 저장
         orderRepository.save(order);
 
         return order.getId();
     }
+
+//    // 주문 목록을 조회하는 로직
+//    @Transactional(readOnly = true)
+//    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+//
+//    }
 }
